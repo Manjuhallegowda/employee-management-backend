ef6cb896fe8c0a5ed2140fa631388b613f5d8b5b
Manjuhallegowda <142666175+Manjuhallegowda@users.noreply.github.com>
2025-06-08 17:17:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://employee-mg.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
